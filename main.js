562f50f34e7a622e0d7914c732d1ff8d4247be49
UZB-AZIMO <abduvorisazimov712@gmail.com>
2024-07-16 22:15:34 +0500

    epk

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ async function fetchUsers(api){
 fetchUsers(API_URL)
 
 const IMAGES = [
-    "https://sun9-6.userapi.com/impg/D3l7eYSAzBffYJw5GxHKsmHiK8I5N4tgDtz56w/zp_8lnYJzVI.jpg?size=1200x752&quality=96&sign=fcdb952f32d113b40ca8b53251de3fd1&c_uniq_tag=hB7yFCR6_KyPVRR7blRBCfGcOO_UlpF2rJSmehmemYg&type=album",
+    "https://i.ytimg.com/vi/9y4Z3k6IjMY/maxresdefault.jpg",
+    "https://www.funnyart.club/uploads/posts/2022-12/1671652727_www-funnyart-club-p-kartinki-devushka-s-telefonom-foni-15.jpg",
     "https://overclockers.ru/st/legacy/blog/121152/387780_O.jpg",
     "https://www.cheltv.ru/wp-content/uploads/2021/04/gelt.jpg",
     "https://i.etsystatic.com/22267623/r/il/579caf/2304686633/il_1588xN.2304686633_hvkq.jpg",
